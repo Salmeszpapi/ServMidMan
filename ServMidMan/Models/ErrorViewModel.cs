508d7a79143c40059ed3da04eb50c6ed8ff160d2
Kristián Salma <salma.krisztian@gmail.com>
2024-01-25 09:50:10 +0100

    Add project files.

---
+++ ServMidMan/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ServMidMan.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
