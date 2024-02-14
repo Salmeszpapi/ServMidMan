1fd9e0442eff6dfe000efaf9edbd0abd4e4c3cf9
Kristián Salma <salma638998@gmail.com>
2024-02-14 15:40:53 +0100

    fix chat.js

--- ServMidMan/wwwroot/lib/microsoft/signalr/dist/browser/chat/chat.js
+++ ServMidMan/wwwroot/lib/microsoft/signalr/dist/browser/chat/chat.js
@@ -17,8 +17,11 @@ connection.on("ReceiveMessage", function (user, message) {
 connection.on("ChangeColor", function (color) {
     var popupMessage = document.getElementById("popup-message");
     popupMessage.style.backgroundColor = "green";
+    var myPopupContainer = document.getElementById("popup-container");
+    myPopupContainer.style.display = "Block";
     setTimeout(function () {
         popupMessage.style.backgroundColor = "white"; // Reset color after 5 seconds
+        myPopupContainer.style.display = "None";
     }, 5000);
 });
 connection.on("NewProductUpdatedReceived", function (user,fero) {
@@ -46,29 +49,14 @@ document.getElementById("ProductUploadedButton").addEventListener("click", funct
 
     // Trigger form submission
     var form = document.getElementById("ProductUploadedButtonForm");
-    var nameInput1 = document.getElementById("ProductUploadedButtonForm1");
-    var nameInput2 = document.getElementById("ProductUploadedButtonForm2");
-    var nameInput3 = document.getElementById("ProductUploadedButtonForm3");
-    var nameInput4 = document.getElementById("ProductUploadedButtonForm4");
-    var nameInput5 = document.getElementById("ProductUploadedButtonForm5");
-    var nameInput6 = document.getElementById("ProductUploadedButtonForm6");
 
-    if (nameInput1.value.trim() === "" ||
-        nameInput2.value.trim() === "" ||
-        nameInput3.value.trim() === "" ||
-        nameInput6.value.trim() === "") {
-        alert("Please fill in all fields before submitting the form.");
-        return; // Exit the function early if any field is empty
-    } else {
-        form.submit();
-    }
+    form.submit();
     var user = "12";
     var message = "1";
 
     var myPopupContainer = document.getElementById("popup-container");
     myPopupContainer.style.display = "Block";
     var myPopup = document.getElementById("popup-message");
-    myPopup.style.background = "red";
     connection.invoke("NewProductUpdated", user, message).catch(function (err) {
         return console.error(err.toString());
     });
